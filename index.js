49d91f4f09de29d6bdfc9ae607ca256b39d1e45b
Paul Yun <p.yun1994@gmail.com>
2019-06-25 18:12:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  let ranked = document.querySelectorAll('.ranked-list > li');
+  for (let i = 0; i < ranked.length; i++){
+    let current = ranked[i];
+    current.innerHTML = parseInt(current.innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
