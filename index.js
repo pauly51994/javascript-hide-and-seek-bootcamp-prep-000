5abed1757bddc191ab19ab17405f06fc0bd705bb
Paul Yun <p.yun1994@gmail.com>
2019-06-25 17:19:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  return document.getElementsByClassName('#nested .target')
+  return document.getElementsByClassName('#nested > .target')
 }
 
 function increaseRankBy(n){
