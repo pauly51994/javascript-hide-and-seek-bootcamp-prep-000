e48a9d74ecf1ff1d6428a45981d13cf60e949f8b
Paul Yun <p.yun1994@gmail.com>
2019-06-25 17:41:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function nestedTarget(){
 function increaseRankBy(n){
   let ranked = document.querySelectorAll('.ranked-list > li');
   for (let i = 0; i < ranked.length; i++){
-    let current = ranked[i];
-    Number(current.innerHTML) += n;
+    let current = ranked[i].innerHTML;
+    Number(current) += n;
   }
 }
 
