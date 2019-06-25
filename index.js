dc2804ff47170b21bec63faa8891b90541b5fc5f
Paul Yun <p.yun1994@gmail.com>
2019-06-25 16:36:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.getElementsByClassName('target')[0]
+}
+
+function increaseRankBy(n){
+  let ranked = document.getElementsByClassName('ranked-list');
+  for (let i = 0; i < ranked.length; i++){
+    
+  }
+}
+
+function deepestChild(){
+  
+}
